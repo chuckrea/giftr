29898b33290d5d9f9d999dff71023ea40333f6ff
chuckrea <chuckr523@mac.com>
2014-02-26 17:08:35 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Giftr::Application.config.secret_key_base = '1133cb172e6b2e4a613a30f09ec40bdb22f9b1cef41c164c674705484f57404da497546724b4292e0b5c9c4a6754b713eb1eb959ec135285ad103007d8dc9f0e'
