39108cea43dbde62ebf4c1cc39292ea626c658fa
Michael Muse <michaelmuse@gmail.com>
2014-02-27 14:16:26 -0500

    more test stuff

--- spec/models/image_spec.rb
+++ spec/models/image_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe Image do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'given two items, one with a url with images and one without images' do
+    before do
+      @item_options1 = {name: "Shiny Necklace", url: ""}
+      @item_options2 = {name: "Beady Necklace", url: ""}
+    end
+    it 'the poll should be able to create items which know which poll they belong to' do
+      @first_poll.createItem(@item_options1)
+      @first_poll.createItem(@item_options2)
+      Item.all.count.should == 2
+      firstItem = Item.all.first
+      firstItem.poll.should == @first_poll
+    end
+  end
 end
