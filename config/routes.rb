33b30bd1a5c761b2916b8e103694d5076e22284a
Michael Muse <michaelmuse@gmail.com>
2014-02-27 08:58:43 -0500

    Adding devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Giftr::Application.routes.draw do
+
+  root :to => 'musicians#index'
+
+  # devise_for :users
+  devise_for :installs
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
