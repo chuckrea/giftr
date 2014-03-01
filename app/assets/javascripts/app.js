40587381a22954f791f3fbe2006d9da737e93ed5
nhindman <nathaniel.hindman@gmail.com>
2014-03-01 16:22:47 -0500

    trying to get poll.save to work and ALMOST THERE GODAMNIT

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -46,6 +46,7 @@ var my_friends = [];
 var exclusions;
 var friend_attrs;
 var accomplices;
+var recipient_name;
 
 $(document).ready(function() {    
 
@@ -86,11 +87,17 @@ $(document).ready(function() {
       closeOnSubmit: true,
       onSubmit: function(response){
         friend_attrs = response;
-        console.log(friend_attrs)
+        // console.log(friend_attrs)
         friend_id = response[0];
 
         mutual_url = '/' + user_id + '/mutualfriends/' + friend_id;
+        FB.api( '/'+friend_id, function(response) {
+          recipient_name = response.name
+        });
 
+        var poll = new Poll({ creator_id: user_id, recipient_name: recipient_name, recipient_photo: "http://graph.facebook.com/friend_id/picture?type=large", recipient_fb_id: friend_id, description: "This Worked!", end_date: "not yet"}).save({success: function(){
+        console.log(poll.description)
+        }})
         // This sets up an array containing ids of mutual friends with your
         // chosen gift recipient.
         // As of now does not work for more than 99 mutual friends
