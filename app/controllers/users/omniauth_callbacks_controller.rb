93972f8e13cd94659b48570832afadf59b7dcee4
chuckrea <chuckr523@mac.com>
2014-03-01 19:10:29 -0500

    don't even know what I'm committing

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -11,7 +11,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session["devise.facebook_data"] = request.env["omniauth.auth"]
       redirect_to new_user_registration_url
     end
-
+    
   end
 
 end
