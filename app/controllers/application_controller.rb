c92084e1073b3bf70c2de0eb97ee6888cba132a0
Michael Muse <michaelmuse@gmail.com>
2014-03-04 16:20:08 -0500

    trying to do show page login redirection stuff

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,9 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:email, :password, :password_confirmation, :name ) }
   end
+
+
+  # def after_sign_in_path_for(resource)
+  #   request.env['omniauth.origin'] || session['login_callback_url'] || root_path
+  # end
 end
