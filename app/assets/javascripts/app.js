3bbfc6fd1ad06543e16a01b49d5aa6b186f1a09d
Zack D <zackdhome@gmail.com>
2014-03-04 22:56:24 -0800

    yeah

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -48,6 +48,7 @@ function resetSelector(){
             // console.log("facebook message fired"),
             method: 'send',
             to: [accomplice],
+            message
             link: "http://giftadvisor.herokuapp.com/polls/" + poll_id,
             redirect_uri: window.location.host+"/polls/"+poll_id  
             }, fbCallback)
